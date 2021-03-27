649e785c0a38892c359464978790eb39dd76954e
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-03-27 18:57:49 +0300

    27/03/2021

--- my_samuray_js/src/components/Users/Users.jsx
+++ my_samuray_js/src/components/Users/Users.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import s from '../Users/Users.module.css';
 import userNoPhoto from '../../assets/img/userNoFoto.png';
+import  * as axios from 'axios';
 
 const Users =(props)=>{
     if(props.users.length===0){
-    props.setUsers( 
-    [{id:1,followed:false,userPhoto:userNoPhoto, fullName:'Alex', status:'I can all do!', location:{city:'Mogilev', country:'Belarus'}},
-        {id:2,followed:true,userPhoto:userNoPhoto, fullName:'Jhon', status:'I need a new job, man! ', location:{city:'Minsk', country:'Belarus'}},
-        {id:3,followed:false,userPhoto:userNoPhoto, fullName:'Will', status:'I very good programmer!', location:{city:'Vitebsk', country:'Belarus'}},
-        {id:4,followed:true,userPhoto:userNoPhoto, fullName:'Joshua', status:'I love beer man!', location:{city:'Kiev', country:'Ukrain'}}
-        ]
-    )
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
+        
+    response =>{
+        props.setUsers(response.data.items)})
+        
 }
 
    
@@ -19,10 +18,10 @@ const Users =(props)=>{
        props.users.map(u=>   
        <div key={u.id} className={s.users}>
            <div>              
-                   <img src={u.userPhoto} className={s.userNoPhotoStyle}></img>
+                   <img src={u.photos.small != null ? u.photos.small :userNoPhoto } className={s.userNoPhotoStyle}></img>
                    <div className={s.aboutUserStyle}>
-                    <div>{u.fullName}</div>
-                    <div>{u.location.city +', ' + u.location.country}</div>
+                    <div>{u.name}</div>
+                    <div>{"u.location.city +', ' + u.location.country"}</div>
                     <br/>
                     <div>{u.status}</div>
                     <div>
