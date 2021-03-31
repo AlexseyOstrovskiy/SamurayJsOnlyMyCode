3a53e3100cef0c8bb8f725f174c9f99709c40d38
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-03-31 22:15:43 +0300

    31/03/2021

--- my_samuray_js/src/components/Users/Users.jsx
+++ my_samuray_js/src/components/Users/Users.jsx
@@ -4,19 +4,24 @@ import userNoPhoto from '../../assets/img/userNoFoto.png';
 import  * as axios from 'axios';
 
 const Users =(props)=>{
+let getUsers =()=>{
     if(props.users.length===0){
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
         
     response =>{
-        props.setUsers(response.data.items)})
-        
-}
+        props.setUsers(response.data.items)}) 
+}}
 
    
     
-    return (
-       props.users.map(u=>   
+    return <div>
+        <div>
+        <button onClick={getUsers}>Get Users</button>
+        </div>
+      { props.users.map(u=>
+        
        <div key={u.id} className={s.users}>
+           
            <div>              
                    <img src={u.photos.small != null ? u.photos.small :userNoPhoto } className={s.userNoPhotoStyle}></img>
                    <div className={s.aboutUserStyle}>
@@ -34,7 +39,8 @@ const Users =(props)=>{
            </div>
            
         </div>
-       )
-    )
+       )}
+    </div>
+    
 }
 export default Users;
